9fe487d5140e91c9281d33ae3f648b62f7cde319
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-06-18 14:56:30 +0100

    Updated controller, routes and created more view files

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class PagesController extends Controller
 {
-    //
+    public function index() 
+    {
+        return view('home');
+    }
+
+    public function about() 
+    {
+        return view('about');
+    }
 }
