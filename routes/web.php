db0458d222c8ead45d3556d21e763ff888166080
Hashaam Sheikh <hashaamsheikh@hotmail.co.uk>
2023-06-18 14:43:03 +0100

    Created controller and defined routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{  
+    Route::get('/', 'PagesController@index')->name('pages.index');
+    Route::get('/about', 'PagesController@about')->name('pages.about');
+});
